5c838ae4f2be06f7531033456cf59f9eade81230
Alexandru <lexyus22@gmail.com>
2026-05-21 18:39:11 +0200

    Add first turtle and betting input.

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ import turtle as t
 tim = t.Turtle()
 screen = t.Screen()
 screen.setup(700,700)
+bet = screen.textinput('Want to give a try ?', 'Make a bet, choose a color: ')
 
+def red_turtle():
+    tim = t.Turtle('turtle')
+    tim.color('red')
+    tim.penup()
+    tim.goto(-300, -200)
+
+red_turtle()
 
 
 
