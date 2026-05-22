7a22d3b34960a4459fe5c315138493751e198f29
Alexandru <lexyus22@gmail.com>
2026-05-22 17:11:10 +0200

    Create turtle racers with starting positions.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import turtle as t
 
-tim = t.Turtle()
 screen = t.Screen()
 screen.setup(700,700)
 bet = screen.textinput('Want to give a try ?', 'Make a bet, choose a color: ')
@@ -9,9 +8,46 @@ def red_turtle():
     tim = t.Turtle('turtle')
     tim.color('red')
     tim.penup()
-    tim.goto(-300, -200)
+    tim.goto(-300, -100)
+
+def orange_turtle():
+    tim = t.Turtle('turtle')
+    tim.color('orange')
+    tim.penup()
+    tim.goto(-300, -50)
+
+def yellow_turtle():
+    tim = t.Turtle('turtle')
+    tim.color('yellow')
+    tim.penup()
+    tim.goto(-300, 0)
+
+def green_turtle():
+    tim = t.Turtle('turtle')
+    tim.color('green')
+    tim.penup()
+    tim.goto(-300, 50)
+
+def blue_turtle():
+    tim = t.Turtle('turtle')
+    tim.color('blue')
+    tim.penup()
+    tim.goto(-300, 100)
+
+def purple_turtle():
+    tim = t.Turtle('turtle')
+    tim.color('purple')
+    tim.penup()
+    tim.goto(-300, 150)
+
+
 
 red_turtle()
+orange_turtle()
+yellow_turtle()
+green_turtle()
+blue_turtle()
+purple_turtle()
 
 
 
